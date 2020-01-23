68f38a6aaf33ca465c9ee03e698480861bf57112
Paweł Banaś <paweloot@gmail.com>
2020-01-23 20:37:34 +0100

    Remove shuffling

--- app/src/main/java/com/paweloot/quiz/extension/QuestionExtensions.kt
+++ app/src/main/java/com/paweloot/quiz/extension/QuestionExtensions.kt
@@ -7,20 +7,17 @@ import com.paweloot.quiz.entity.SoundtrackQuestion
 fun PhotoQuestion.allAnswers(): List<String> {
     return wrongAnswers.toMutableList().also {
         it.add(photo.answer)
-        it.shuffle()
     }
 }
 
 fun SoundtrackQuestion.allAnswers(): List<String> {
     return wrongAnswers.toMutableList().also {
         it.add(soundtrack.soundtrackTitle)
-        it.shuffle()
     }
 }
 
 fun ClipQuestion.allAnswers(): List<String> {
     return wrongAnswers.toMutableList().also {
         it.add(clip.movieTitle)
-        it.shuffle()
     }
 }

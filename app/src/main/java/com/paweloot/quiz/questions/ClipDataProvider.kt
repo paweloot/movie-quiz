4710bca0c4be3822e1ed0f46a1de4603883c4e89
Paweł Banaś <paweloot@gmail.com>
2020-01-23 18:49:45 +0100

    Fix clip asset paths

--- app/src/main/java/com/paweloot/quiz/questions/ClipDataProvider.kt
+++ app/src/main/java/com/paweloot/quiz/questions/ClipDataProvider.kt
@@ -9,35 +9,35 @@ object ClipDataProvider {
         ClipData(
             "Whiplash",
             "Damien Chazelle",
-            "whiplash.mp4",
+            "clips/whiplash.mp4",
             "https://images-na.ssl-images-amazon.com/images/I/516noX3aE9L.jpg",
             2014
         ),
         ClipData(
             "Forrest Gump",
             "Robert Zemeckis",
-            "forrest_gump.mp4",
+            "clips/forrest_gump.mp4",
             "https://i.etsystatic.com/15375993/r/il/cbafe5/1303342253/il_570xN.1303342253_qphh.jpg",
             1994
         ),
         ClipData(
             "Intouchables",
             "Olivier Nakache, Éric Toledano",
-            "intouchables.mp4",
+            "clips/intouchables.mp4",
             "https://images-na.ssl-images-amazon.com/images/I/51WbHjvEceL.jpg",
             2011
         ),
         ClipData(
             "Gladiator",
             "Ridley Scott",
-            "gladiator.mp4",
+            "clips/gladiator.mp4",
             "https://images-na.ssl-images-amazon.com/images/I/51o9U06EV8L._AC_.jpg",
             2000
         ),
         ClipData(
             "The Shawshank Redemption",
             "Frank Darabont",
-            "shawshank.mp4",
+            "clips/shawshank.mp4",
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkmMH-bEDUS2TmK8amBqgIMgrfzN1_mImChPuMrunA1XjNTSKm",
             1994
         )

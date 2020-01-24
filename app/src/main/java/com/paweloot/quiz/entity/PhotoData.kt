348e49844dcb48524bf59cf881cfb8142d956833
Paweł Banaś <paweloot@gmail.com>
2020-01-24 22:33:26 +0100

    Disable Glide's cache

--- app/src/main/java/com/paweloot/quiz/entity/PhotoData.kt
+++ app/src/main/java/com/paweloot/quiz/entity/PhotoData.kt
@@ -4,6 +4,7 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 
 data class PhotoData(
     val photoUrl: String,
@@ -14,11 +15,13 @@ data class PhotoData(
 fun loadImage(view: ImageView, url: String) {
     val circularProgressDrawable = CircularProgressDrawable(view.context)
     circularProgressDrawable.strokeWidth = 5f
-    circularProgressDrawable.centerRadius = 30f
+    circularProgressDrawable.centerRadius = 60f
     circularProgressDrawable.start()
 
     Glide.with(view.context)
         .load(url)
         .placeholder(circularProgressDrawable)
+        .diskCacheStrategy(DiskCacheStrategy.NONE)
+        .skipMemoryCache(true)
         .into(view)
 }
